0e47fb3b715f61a6c69bf46ca0ea2839b8a41b47
Flavio De Stefano <destefano.flavio@gmail.com>
2026-06-14 14:23:03 +0200

    fix: use permissive ALL:@SECLEVEL=0 ciphers for broad Tapo compatibility

--- src/tapoCamera.ts
+++ src/tapoCamera.ts
@@ -72,7 +72,7 @@ export class TAPOCamera extends OnvifCamera {
       connect: {
         // TAPO devices have self-signed certificates
         rejectUnauthorized: false,
-        ciphers: "AES256-SHA:AES128-GCM-SHA256",
+        ciphers: "ALL:@SECLEVEL=0",
       },
     });
 
